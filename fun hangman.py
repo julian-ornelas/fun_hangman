ec1a711b50d0d69cde1200ae85fa480230ddbf6c
julian-ornelas <122693339+julian-ornelas@users.noreply.github.com>
2023-03-01 11:55:40 -0500

    Add files via upload
    Base design of the hangman game. Currently has a bug where the game does not register the same letters multiple times (e.g., an input of "o" while the word is "loops" will be "_ o _ _ _")

---
+++ fun hangman.py
@@ -0,0 +1,65 @@
+import requests
+import json
+import random
+
+# API request which turns JSON file into string
+difficulty = input("Difficulty (easy, medium, or hard): ")
+if difficulty == "easy":
+    number = str(random.randint(4, 6))
+    word_request = requests.get("https://random-word-api.herokuapp.com/word?length=" + number)
+elif difficulty == "medium":
+    number = str(random.randint(7, 10))
+    word_request = requests.get("https://random-word-api.herokuapp.com/word?length=" + number)
+elif difficulty == "hard":
+    number = str(random.randint(11, 15))
+    word_request = requests.get("https://random-word-api.herokuapp.com/word?length=" + number)
+else:
+    word_request = requests.get("https://random-word-api.herokuapp.com/word?length=")
+
+# Converts json into readable string
+word = json.loads(word_request.text)
+word = json.dumps(word)
+word = word[2:-2]
+print(word)
+print(number)
+
+decompsed_word = list(word)
+print(decompsed_word)
+
+# Length
+length = len(word)
+print(f"{length} characters")
+
+# Prints the spaces for player.
+play_area = ["_"]
+play_area = play_area * length
+
+# Starts off the player with 8 lives.
+player_lives = 8
+print(f"You have {player_lives} lives")
+
+print(play_area)
+
+# Checks for input letter against the generated word
+# Shows letter in the play area
+# Counts lives accordingly
+while player_lives != 0:
+
+    player_guess = input("Letter: ")
+    if player_guess in word:
+        guess_location = word.find(player_guess)
+        play_area[guess_location] = player_guess
+        print("Yes")
+        if play_area == decompsed_word:
+            print("You win!")
+            break
+    else:
+        player_lives -= 1
+        print("No")
+
+    
+    print(f"{player_lives} lives remain")
+    print(play_area)
+
+# Prints word at the very end.
+print(f"The word was {word}.")
